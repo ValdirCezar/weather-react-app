be88ab162c8c1f1cc274342408b39e12013f539b
Valdir Cezar <valdircezar312@gmail.com>
2021-04-20 17:00:56 -0300

    feat: add Menu component

---
+++ src/components/Menu/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Menu () {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={ () => navigation.openDrawer() }>
+      <Entypo
+        name="menu"
+        size={36}
+        color="#373737"
+      />
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    zIndex: 9,
+    width: 70,
+    height: 70,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: 15,
+    top: 40,
+    borderTopRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    elevation: 25,
+    shadowColor: '#000',
+    shadowOpacity: 0.6,
+    shadowOffset: {
+      height: 10,
+      width: 10
+    }
+  }
+});
