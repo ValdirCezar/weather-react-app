1f4094dd3c2b37e180e68c187aa7516726b51925
Valdir Cezar <valdircezar312@gmail.com>
2021-04-20 21:11:27 -0300

    feat: create Home component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,48 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
+import { Ionicons } from '@expo/vector-icons';
+
 export default function Header () {
   return (
-    <View style={styles.container}>
-      <Text> Página Header OK </Text>
-    </View>
+    <LinearGradient style={styles.header} colors={['#1ed6ff', '#97c1ff']} >
+      <Text style={styles.date}>12/02/2021</Text>
+      <Text style={styles.city}>Londrina</Text>
+
+        <Ionicons
+          name="cloud"
+          color="#FFF"
+          size={150}
+        />
+
+      <Text style={styles.degree}>30°C</Text>
+
+    </LinearGradient>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, // Pega o tamanho inteiro da tela
+  header: {
+    width: "95%",
+    height: "55%",
+    alignContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
+    top: 25
+  },
+  date: {
+    color: "#FFF",
+    fontWeight: 'bold',
+    marginTop: 25
+  },
+  city: {
+    color: "#FFF",
+    fontWeight: 'bold',
+    fontSize: 25
+  },
+  degree: {
+    fontSize: 80,
+    color: "#FFF",
+    fontWeight: 'bold'
   }
 });
