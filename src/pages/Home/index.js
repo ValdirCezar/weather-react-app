64203c442eae7e1109efa91e876d8b4aec563ac1
Valdir Cezar <valdircezar312@gmail.com>
2021-04-21 21:26:39 -0300

    feat: render ficticy datas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,111 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, FlatList } from 'react-native';
 
 import Menu from '../../components/Menu'
 import Header from '../../components/Header';
 import Conditions from '../../components/Conditions';
+import Forecast from '../../components/Forecast';
 
-export default function Home() {
+const mylist = [
+  {
+    "date": "04-20",
+    "weekday": "Tue",
+    "max": 20,
+    "min": 15,
+    "description": "Showers",
+    "condition": "clear_day"
+  },
+  {
+    "date": "04-21",
+    "weekday": "Wed",
+    "max": 20,
+    "min": 15,
+    "description": "Showers",
+    "condition": "rain"
+  },
+  {
+    "date": "04-22",
+    "weekday": "Thu",
+    "max": 20,
+    "min": 15,
+    "description": "Day partly cloudy",
+    "condition": "cloudly_day"
+  },
+  {
+    "date": "04-23",
+    "weekday": "Fri",
+    "max": 22,
+    "min": 14,
+    "description": "Day partly cloudy",
+    "condition": "cloudly_day"
+  },
+  {
+    "date": "04-24",
+    "weekday": "Sat",
+    "max": 25,
+    "min": 14,
+    "description": "Day partly cloudy",
+    "condition": "cloudly_day"
+  },
+  {
+    "date": "04-25",
+    "weekday": "Sun",
+    "max": 25,
+    "min": 15,
+    "description": "Scattered thunderstorms",
+    "condition": "storm"
+  },
+  {
+    "date": "04-26",
+    "weekday": "Mon",
+    "max": 22,
+    "min": 16,
+    "description": "Showers",
+    "condition": "rain"
+  },
+  {
+    "date": "04-27",
+    "weekday": "Tue",
+    "max": 19,
+    "min": 13,
+    "description": "Day partly cloudy",
+    "condition": "cloudly_day"
+  },
+  {
+    "date": "04-28",
+    "weekday": "Wed",
+    "max": 18,
+    "min": 13,
+    "description": "Day partly cloudy",
+    "condition": "cloudly_day"
+  },
+  {
+    "date": "04-29",
+    "weekday": "Thu",
+    "max": 18,
+    "min": 12,
+    "description": "Day partly cloudy",
+    "condition": "cloudly_day"
+  }
+]
+
+export default function Home () {
   return (
     <SafeAreaView style={styles.container}>
 
-      <Menu/>
-      <Header/>
-      <Conditions/>
-      
+      <Menu />
+      <Header />
+      <Conditions />
+
+      <FlatList
+        horizontal={true}
+        style={styles.list}
+        contentContainerStyle={{paddingBottom: '5%'}}
+        data={mylist}
+        keyExtractor={item => item.data}
+        renderItem={({ item }) => <Forecast data={item} />}
+      />
+
     </SafeAreaView>
   )
 }
@@ -24,5 +117,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#E8f0ff',
     paddingTop: '5%'
+  },
+  list: {
+    marginTop: 10,
+    marginLeft: 10
   }
 });
