f8d7a3875dad90df819fc5d2a4b6f860f3a3f574
Valdir Cezar <valdircezar312@gmail.com>
2021-04-21 21:27:22 -0300

    feat: add condition function to help a choice correct icons

---
+++ src/utils/condition.js
@@ -0,0 +1,33 @@
+export function condition (condition) {
+  switch (condition) {
+    case 'storm':
+      return icon = {
+        name: 'rainy-outline',
+        color: '#1ec9ff'
+      };
+
+    case 'clear_day':
+      return icon = {
+        name: 'partly-sunny-outline',
+        color: '#FFB300'
+      };
+
+    case 'storm':
+      return icon = {
+        name: 'rainy-outline',
+        color: '#1ec9ff'
+      };
+
+    case 'rain':
+      return icon = {
+        name: 'rainy-outline',
+        color: '#1ec9ff'
+      };
+
+    default:
+      return icon = {
+        name: 'cloud-outline',
+        color: '#1ec9ff'
+      }
+  }
+}
