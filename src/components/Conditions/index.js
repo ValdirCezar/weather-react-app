76ffd509ce8fa5a0e8875b1133bf8dd2a09fd039
Valdir Cezar <valdircezar312@gmail.com>
2021-04-21 21:25:04 -0300

    chore: adjusts in Forecast

--- src/components/Conditions/index.js
+++ src/components/Conditions/index.js
@@ -7,26 +7,25 @@ export default function Conditions () {
   return (
     <View style={styles.container}>
 
-      <View style={styles.contenten}>
+      <View style={styles.content}>
         <Feather name='wind' size={23} color="#1ed6ff" />
-        <Text style={{fontWeight: 'bold'}}>9 km/h</Text>
+        <Text style={{ fontWeight: 'bold' }}>9 km/h</Text>
       </View>
 
-      <View style={styles.contenten}>
+      <View style={styles.content}>
         <MaterialCommunityIcons name='weather-sunset-up' size={23} color="#1ed6ff" />
-        <Text style={{fontWeight: 'bold'}}>5:22 am</Text>
+        <Text style={{ fontWeight: 'bold' }}>5:22 am</Text>
       </View>
 
-      <View style={styles.contenten}>
+      <View style={styles.content}>
         <MaterialCommunityIcons name='weather-sunset-down' size={23} color="#1ed6ff" />
-        <Text style={{fontWeight: 'bold'}}>6:22 pm</Text>
+        <Text style={{ fontWeight: 'bold' }}>6:22 pm</Text>
       </View>
 
-      <View style={styles.contenten}>
+      <View style={styles.content}>
         <Feather name='droplet' size={23} color="#1ed6ff" />
-        <Text style={{fontWeight: 'bold'}}>69</Text>
+        <Text style={{ fontWeight: 'bold' }}>69</Text>
       </View>
-
 
     </View>
   )
@@ -39,9 +38,10 @@ const styles = StyleSheet.create({
     padding: 10,
     flexDirection: 'row',
     width: "95%",
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    borderRadius: 9
   },
-  contenten: {
+  content: {
     justifyContent: 'center',
     alignItems: 'center'
   }
